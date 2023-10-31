d05ef1fd5dffefe1ffbde9fa850cd0b516ff3791
ced0-tech <cedric.michael@student.moringaschool.com>
2023-10-31 17:05:49 +0300

    Homepage n organization

---
+++ src/ContactUs.jsx
@@ -0,0 +1,243 @@
+import React from "react";
+
+export const ContactUs = () => {
+  return (
+    <div className="bg-[#f1f1f1] flex flex-row justify-center w-full">
+      <div className="bg-[#f1f1f1] overflow-hidden w-[1924px] h-[2912px] relative">
+        <div className="absolute w-[1926px] h-[633px] top-0 left-0">
+          <div className="absolute w-[1926px] h-[557px] top-[76px] left-0">
+            <div className="relative w-[1924px] h-[557px] bg-[url(xabpx-2unyngzbhzoaqu9yg8lyp6w02z-icjyrukkfa-1.png)] bg-cover bg-[50%_50%]">
+              <div className="absolute w-[400px] top-[231px] left-[280px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[64px] tracking-[0] leading-[normal]">
+                CONTACT
+              </div>
+              <img
+                className="absolute w-[165px] h-[2px] top-[269px] left-[58px] object-cover"
+                alt="Line"
+                src="line-2.svg"
+              />
+              <img
+                className="absolute w-[165px] h-[2px] top-[269px] left-[736px] object-cover"
+                alt="Line"
+                src="line-3.svg"
+              />
+            </div>
+          </div>
+          <div className="absolute w-[1921px] h-[86px] top-0 left-[3px]">
+            <div className="w-[1923px] h-[86px]">
+              <div className="relative w-[1921px] h-[86px]">
+                <img className="absolute w-[1385px] h-[75px] top-px left-0" alt="Rectangle" src="rectangle-44.svg" />
+                <div className="absolute w-[673px] h-[26px] top-[16px] left-[398px]">
+                  <div className="w-[95px] top-px left-0 absolute [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+                    Home
+                  </div>
+                  <div className="absolute w-[95px] top-0 left-[147px] [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+                    About
+                  </div>
+                  <div className="absolute w-[216px] top-0 left-[244px] [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+                    Organizations
+                  </div>
+                  <div className="w-[142px] top-0 left-[523px] absolute [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+                    Contact
+                  </div>
+                </div>
+                <img
+                  className="absolute w-[705px] h-[76px] top-0 left-[1216px]"
+                  alt="Rectangle"
+                  src="rectangle-45.svg"
+                />
+                <div className="absolute w-[137px] top-[12px] left-[1613px] [font-family:'Inter-SemiBold',Helvetica] font-semibold text-black text-[34px] tracking-[0] leading-[normal]">
+                  Donate
+                </div>
+                <div className="absolute w-[226px] h-[78px] top-[8px] left-[39px]">
+                  <p className="absolute w-[222px] top-0 left-0 [font-family:'Kyiv*Type_Titling-Medium',Helvetica] font-normal text-transparent text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+                    <span className="font-medium text-white">MAZI</span>
+                    <span className="[font-family:'Inter-SemiBold',Helvetica] font-semibold text-[#73b925]">NGIRA</span>
+                  </p>
+                  <div className="absolute w-[222px] top-[39px] left-0 [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[18px] text-center tracking-[0] leading-[normal]">
+                    Nature &amp; sustainability
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute w-[1016px] h-[822px] top-[770px] left-0 bg-[#1c1c1c]">
+          <div className="absolute w-[466px] top-[20px] left-[283px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[64px] tracking-[0] leading-[normal] whitespace-nowrap">
+            GET IN TOUCH
+          </div>
+          <img className="absolute w-[136px] h-[7px] top-[83px] left-[566px]" alt="Line" src="line-4.svg" />
+          <div className="absolute w-[419px] h-[96px] top-[155px] left-[578px] bg-white rounded-[10px]">
+            <div className="top-[14px] left-[15px] absolute w-[95px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[14px] tracking-[0] leading-[normal]">
+              Your Email
+            </div>
+          </div>
+          <div className="absolute w-[366px] h-[96px] top-[157px] left-[14px] bg-white rounded-[10px]">
+            <div className="top-[16px] left-[17px] absolute w-[95px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[14px] tracking-[0] leading-[normal]">
+              Your name
+            </div>
+          </div>
+          <div className="absolute w-[838px] h-[226px] top-[279px] left-[71px] bg-white rounded-[10px]">
+            <div className="absolute w-[95px] top-[31px] left-[8px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[14px] tracking-[0] leading-[normal]">
+              Message
+            </div>
+          </div>
+          <div className="absolute w-[426px] h-[67px] top-[531px] left-[329px] bg-[#73b925] rounded-[120px]">
+            <div className="absolute w-[196px] top-[15px] left-[115px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[26px] tracking-[0] leading-[normal]">
+              Send Message
+            </div>
+          </div>
+        </div>
+        <div className="absolute w-[793px] h-[822px] top-[708px] left-[1131px] bg-[url(rectangle-118.svg)] bg-[100%_100%]">
+          <p className="absolute w-[731px] top-[84px] left-[21px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[26px] tracking-[0] leading-[normal]">
+            <span className="[font-family:'Inter-Bold',Helvetica] font-bold text-white text-[26px] tracking-[0]">
+              Contact Us
+              <br />
+              Have a question, comment, or suggestion? Feel free to reach out to us using the following email addresses:
+              <br />
+              General Inquiries:{" "}
+            </span>
+            <a href="mailto:cedric@gmail.com" rel="noopener noreferrer" target="_blank">
+              <span className="underline">
+                cedric@gmail.com
+                <br />
+              </span>
+            </a>
+            <span className="[font-family:'Inter-Bold',Helvetica] font-bold text-white text-[26px] tracking-[0]">
+              Customer Support:{" "}
+            </span>
+            <a href="mailto:miriam@gmail.com" rel="noopener noreferrer" target="_blank">
+              <span className="underline">
+                miriam@gmail.com
+                <br />
+              </span>
+            </a>
+            <span className="[font-family:'Inter-Bold',Helvetica] font-bold text-white text-[26px] tracking-[0]">
+              Business Partnerships:{" "}
+            </span>
+            <a href="mailto:Victor@gmail.com" rel="noopener noreferrer" target="_blank">
+              <span className="underline">
+                Victor@gmail.com
+                <br />
+              </span>
+            </a>
+            <span className="[font-family:'Inter-Bold',Helvetica] font-bold text-white text-[26px] tracking-[0]">
+              We value your feedback and will do our best to respond to your inquiries as soon as possible.
+              <br />
+              Address: 123 Main Street, Cityville, State, ZIP Code
+              <br />
+              Phone: (123) 456-7890
+              <br />
+              Business Hours: Monday - Friday: 9:00 AM - 5:00 PM
+              <br />
+              Thank you for reaching out to us!
+              <br />
+              <br />
+            </span>
+          </p>
+        </div>
+        <div className="absolute w-[1948px] h-[627px] top-[2286px] left-0">
+          <div className="absolute w-[1924px] h-[374px] top-[169px] left-0">
+            <div className="absolute w-[860px] h-px top-[357px] left-[114px] bg-[#53535366]" />
+            <img
+              className="w-[1924px] h-[374px] object-cover absolute top-0 left-0"
+              alt="Hands with plants"
+              src="hands-with-plants-1280-1.png"
+            />
+            <div className="absolute w-[215px] h-[53px] top-[252px] left-[209px]">
+              <img
+                className="absolute w-[50px] h-[50px] top-px left-[81px] object-cover"
+                alt="Instagram"
+                src="instagram-1.png"
+              />
+              <img
+                className="absolute w-[52px] h-[51px] top-0 left-[163px] object-cover"
+                alt="Twitter sign"
+                src="twitter-sign-1.png"
+              />
+              <img
+                className="absolute w-[54px] h-[53px] top-0 left-0 object-cover"
+                alt="Facebook"
+                src="facebook-1.png"
+              />
+            </div>
+            <div className="absolute w-[260px] h-[95px] top-[23px] left-[209px]">
+              <p className="absolute w-[256px] top-0 left-0 [font-family:'Kyiv*Type_Titling-Medium',Helvetica] font-normal text-transparent text-[34px] text-center tracking-[0] leading-[normal]">
+                <span className="font-medium text-white">MAZI</span>
+                <span className="[font-family:'Inter-SemiBold',Helvetica] font-semibold text-[#73b925]">NGIRA</span>
+              </p>
+              <div className="absolute w-[256px] top-[47px] left-0 [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[18px] text-center tracking-[0] leading-[normal]">
+                Nature &amp; sustainability
+              </div>
+            </div>
+            <p className="absolute w-[335px] top-[82px] left-[1066px] [font-family:'Kyiv*Type_Titling-Bold',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
+              <span className="font-bold">
+                Have a Question?
+                <br />
+              </span>
+              <span className="[font-family:'Kyiv*Type_Titling-Regular',Helvetica]">
+                Park-Road, Kilimani
+                <br />
+                +254 721 526852
+                <br />
+                mazingira.sustain@gmai.org
+              </span>
+            </p>
+            <img
+              className="absolute w-[348px] h-[313px] top-[24px] left-[1398px] object-cover"
+              alt="Rectangle"
+              src="rectangle-62.png"
+            />
+            <p className="absolute w-[356px] top-[116px] left-[217px] [font-family:'Kyiv*Type_Titling-Medium',Helvetica] font-medium text-white text-[18px] tracking-[0] leading-[normal]">
+              A small river named Duden flows by their place and supplies it with the necessary regelialia.
+            </p>
+            <p className="absolute w-[184px] top-[63px] left-[594px] [font-family:'Kyiv*Type_Titling-Medium',Helvetica] font-medium text-white text-[18px] tracking-[0] leading-[normal]">
+              <span className="[font-family:'Kyiv*Type_Titling-Medium',Helvetica] font-medium text-white text-[18px] tracking-[0]">
+                Services
+                <br />
+                Recycling
+                <br />
+                Water Refine
+                <br />
+                Ecosystem
+                <br />
+                Solar Enerfy
+              </span>
+            </p>
+            <p className="absolute w-[242px] top-[90px] left-[815px] [font-family:'Kyiv*Type_Titling-Medium',Helvetica] font-medium text-white text-[18px] tracking-[0] leading-[normal]">
+              Save Animals
+              <br />
+              Plant Ecology
+              <br />
+              Dynamic Ecology
+              <br />
+              Tree Planting
+            </p>
+          </div>
+          <div className="absolute w-[1923px] h-[84px] top-[542px] left-px bg-[url(rectangle-35.svg)] bg-[100%_100%]">
+            <p className="absolute w-[437px] top-[27px] left-[763px] [font-family:'Inter-Regular',Helvetica] font-normal text-white text-[20px] text-right tracking-[0] leading-[normal]">
+              Copyright ©2023 All rights reserved |
+            </p>
+          </div>
+          <div className="absolute w-[1934px] h-[169px] top-0 left-0">
+            <img
+              className="w-[1924px] h-[169px] object-cover absolute top-0 left-0"
+              alt="Hands with plants"
+              src="hands-with-plants-1280-2.png"
+            />
+            <div className="w-[1934px] h-[169px] bg-[#73b925ed] absolute top-0 left-0" />
+            <div className="absolute w-[626px] top-[62px] left-[191px] [font-family:'Inter-Regular',Helvetica] font-normal text-white text-[40px] text-right tracking-[0] leading-[normal]">
+              Subscribe For Newsletter
+            </div>
+            <div className="absolute w-[523px] h-[67px] top-[52px] left-[992px] bg-[#f1f1f1] rounded-[25px_0px_0px_25px]" />
+            <div className="absolute w-[232px] h-[67px] top-[52px] left-[1515px] bg-white" />
+            <div className="absolute w-[146px] top-[74px] left-[1551px] [font-family:'Inter-SemiBold',Helvetica] font-semibold text-black text-[13px] text-center tracking-[0] leading-[normal]">
+              SUBSCRIBE NOW!
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
---
+++ src/Organization.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+
+export const Organization = () => {
+  return (
+    <div className="bg-white flex flex-row justify-center w-full">
+      <div className="bg-white overflow-hidden w-[1924px] h-[2912px] relative">
+        <div className="absolute w-[697px] h-[1234px] top-[270px] left-[105px]">
+          <div className="absolute w-[697px] h-[1205px] top-[29px] left-0 bg-[#f1f1f1]" />
+          <img
+            className="absolute w-[696px] h-[645px] top-0 left-0 object-cover"
+            alt="Rectangle"
+            src="rectangle-64.png"
+          />
+          <p className="absolute w-[213px] top-[693px] left-[242px] [font-family:'Inter-Regular',Helvetica] font-normal text-transparent text-[16px] tracking-[0] leading-[normal]">
+            <span className="text-black">Group Goal: </span>
+            <span className="text-[#73b925]">34000.00 USD</span>
+          </p>
+          <p className="absolute w-[243px] top-[742px] left-[223px] [font-family:'Inter-Regular',Helvetica] font-normal text-transparent text-[16px] tracking-[0] leading-[normal]">
+            <span className="text-black">Amount Raised:</span>
+            <span className="text-[#73b925]"> 15000.00 USD</span>
+          </p>
+          <p className="absolute w-[243px] top-[793px] left-[252px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[16px] tracking-[0] leading-[normal]">
+            Help us attain this goal !
+          </p>
+          <img className="absolute w-[368px] h-px top-[844px] left-[157px] object-cover" alt="Line" src="line-1.svg" />
+        </div>
+        <div className="absolute w-[286px] top-[269px] left-[983px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[16px] tracking-[0] leading-[normal]">
+          Published March23, 2023
+        </div>
+        <div className="absolute w-[372px] top-[315px] left-[983px] [font-family:'Inter-SemiBold',Helvetica] font-semibold text-black text-[34px] tracking-[0] leading-[normal] whitespace-nowrap">
+          Project Organic Farms
+        </div>
+        <div className="absolute w-[1924px] h-[116px] top-[4px] left-0 bg-[#1c1c1c]">
+          <img
+            className="absolute w-[68px] h-[65px] top-[31px] left-[1537px] object-cover"
+            alt="Location"
+            src="location-1.png"
+          />
+          <div className="absolute w-[222px] h-[78px] top-[38px] left-[179px]">
+            <p className="absolute w-[218px] top-0 left-0 [font-family:'Kyiv*Type_Titling-Medium',Helvetica] font-normal text-transparent text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+              <span className="font-medium text-white">MAZI</span>
+              <span className="[font-family:'Inter-SemiBold',Helvetica] font-semibold text-[#73b925]">NGIRA</span>
+            </p>
+            <div className="absolute w-[218px] top-[39px] left-0 [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[18px] text-center tracking-[0] leading-[normal]">
+              Nature &amp; sustainability
+            </div>
+          </div>
+          <div className="absolute w-[207px] h-[55px] top-[43px] left-[1161px]">
+            <img
+              className="absolute w-[57px] h-[55px] top-0 left-0 object-cover"
+              alt="Phone receiver"
+              src="phone-receiver-silhouette-1.png"
+            />
+            <div className="absolute w-[106px] top-[5px] left-[43px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#73b925] text-[16px] text-center tracking-[0] leading-[normal]">
+              CALL US:
+            </div>
+            <div className="absolute w-[153px] top-[31px] left-[54px] [font-family:'Inter-Regular',Helvetica] font-normal text-white text-[16px] text-center tracking-[0] leading-[normal]">
+              +254 721 526852
+            </div>
+          </div>
+          <div className="absolute w-[314px] h-[49px] top-[49px] left-[759px]">
+            <div className="absolute w-[145px] h-[49px] top-0 left-0">
+              <img
+                className="absolute w-[51px] h-[49px] top-0 left-0 object-cover"
+                alt="Paper plane"
+                src="paper-plane-2-1.png"
+              />
+              <div className="absolute w-[106px] top-0 left-[40px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#73b925] text-[16px] text-center tracking-[0] leading-[normal]">
+                EMAIL:
+              </div>
+            </div>
+            <div className="absolute w-[224px] top-[25px] left-[90px] [font-family:'Inter-Regular',Helvetica] font-normal text-white text-[16px] text-center tracking-[0] leading-[normal]">
+              mazingira.sustain@gmai.org
+            </div>
+          </div>
+          <div className="absolute w-[106px] top-[48px] left-[1631px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#73b925] text-[16px] text-center tracking-[0] leading-[normal]">
+            LOCATION:
+          </div>
+          <div className="absolute w-[170px] top-[74px] left-[1633px] [font-family:'Inter-Regular',Helvetica] font-normal text-white text-[16px] text-center tracking-[0] leading-[normal]">
+            NgongLane, Nairobi
+          </div>
+        </div>
+        <div className="absolute w-[1920px] h-[76px] top-[125px] left-[4px]">
+          <img className="absolute w-[1408px] h-[76px] top-0 left-0" alt="Rectangle" src="rectangle-44.svg" />
+          <div className="absolute w-[692px] h-[26px] top-[16px] left-[178px]">
+            <div className="w-[94px] top-px left-0 absolute [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+              Home
+            </div>
+            <div className="absolute w-[94px] top-0 left-[145px] [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+              About
+            </div>
+            <div className="absolute w-[213px] top-0 left-[269px] [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+              Organizations
+            </div>
+            <div className="w-[140px] top-0 left-[543px] absolute [font-family:'Abel-Regular',Helvetica] font-normal text-white text-[34px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+              Contact
+            </div>
+          </div>
+          <img className="absolute w-[709px] h-[76px] top-0 left-[1211px]" alt="Rectangle" src="rectangle-45.svg" />
+          <div className="w-[135px] top-[11px] left-[1785px] [font-family:'Inter-SemiBold',Helvetica] font-semibold text-[34px] absolute text-black tracking-[0] leading-[normal]">
+            Donate
+          </div>
+        </div>
+        <div className="absolute w-[272px] h-[250px] top-[964px] left-[1265px] bg-[#f1f1f1]">
+          <img
+            className="absolute w-[80px] h-[89px] top-[84px] left-[27px] object-cover"
+            alt="Heart"
+            src="heart-2.png"
+          />
+          <div className="w-[128px] top-[110px] left-[144px] [font-family:'Inter-Bold',Helvetica] font-bold text-[20px] absolute text-black tracking-[0] leading-[normal]">
+            Donate Now
+          </div>
+        </div>
+        <p className="absolute w-[842px] top-[377px] left-[982px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[20px] tracking-[0] leading-[normal]">
+          At Mazingira, we&#39;re committed to environmental preservation through sustainable practices, including
+          organic farming. Here&#39;s what our approach entails:
+          <br />
+          <br />
+          Healthy Soil: We prioritize soil health with natural methods like composting and crop rotation.
+          <br />
+          Chemical-Free: Our farms are entirely chemical-free, promoting natural pest control and biodiversity.
+          <br />
+          Non-GMO: We ensure our crops are non-GMO to preserve genetic diversity.
+          <br />
+          Animal Welfare: We prioritize humane treatment of our livestock.
+          <br />
+          Sustainability: From water use to energy efficiency, we practice sustainable farming.
+          <br />
+          Community Building: We collaborate with local farmers, support fair labor practices, and empower communities.
+          <br />
+          <br />
+          Mazingira is all about nurturing the environment, food, and communities through organic farming
+        </p>
+      </div>
+    </div>
+  );
+};

05df862f1b55fc0e94a680b02a88cd8dd6230450
ced0-tech <cedric.michael@student.moringaschool.com>
2023-11-07 15:59:43 +0300

    new codes

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,211 +1,45 @@
-import React from "react";
+import React from 'react';
+import './App.css';
 
-export const LandingPage = () => {
-  return (
-    <div className="landing-page">
-      <div className="div">
-        <div className="overlap">
-          <div className="group">
-            <div className="overlap-group">
-              <div className="overlap-2">
-                <div className="rectangle" />
-                <div className="text-wrapper">TRANSFORM THE WORLD</div>
-              </div>
-              <div className="text-wrapper-2">Protect Organic Farms</div>
-            </div>
-          </div>
-          <div className="rectangle-2" />
-          <div className="text-wrapper-3">GET STARTED</div>
-          <img className="arrow" alt="Arrow" src="src/assets/arrow-1.svg" />
-        </div>
-        <div className="overlap-3">
-          <div className="text-wrapper-4">Donate</div>
-          <div className="group-2">
-            <div className="overlap-4">
-              <img className="img" alt="Rectangle" src="src/assets/rectangle-51.png" />
-              <div className="text-wrapper-5">Recycling is the Answer</div>
-              <p className="GROUP-GOAL">
-                GROUP GOAL: 26000.00 USD
-                <br />
-                RAISED: 11500.00 USD (44.23%)
-              </p>
-            </div>
-            <div className="overlap-group-wrapper">
-              <div className="overlap-group-2">
-                <img className="img" alt="Rectangle" src="src/assets/image.png" />
-                <div className="text-wrapper-6">Recycling is the Answer</div>
-                <p className="GROUP-GOAL">
-                  GROUP GOAL: 26000.00 USD
-                  <br />
-                  RAISED: 11500.00 USD (44.23%)
-                </p>
-              </div>
-            </div>
-            <div className="overlap-5">
-              <div className="overlap-wrapper">
-                <div className="overlap-group-2">
-                  <div className="text-wrapper-6">Recycling is the Answer</div>
-                  <p className="GROUP-GOAL">
-                    GROUP GOAL: 26000.00 USD
-                    <br />
-                    RAISED: 11500.00 USD (44.23%)
-                  </p>
-                </div>
-              </div>
-              <div className="group-3" />
-            </div>
-          </div>
-          <div className="rectangle-3" />
-          <div className="rectangle-4" />
-          <div className="rectangle-5" />
-          <div className="text-wrapper-7">Donate</div>
-          <div className="text-wrapper-8">Donate</div>
-          <div className="text-wrapper-9">Donate</div>
-        </div>
-        <div className="div-wrapper">
-          <div className="overlap-6">
-            <p className="over-people">
-              <span className="span">Over </span>
-              <span className="text-wrapper-10">40K+</span>
-              <span className="span"> People Working</span>
-            </p>
-            <div className="text-wrapper-11">With us</div>
-          </div>
-        </div>
-        <div className="text-wrapper-12">Services</div>
-        <div className="rectangle-6" />
-        <div className="overlap-7">
-          <div className="text-wrapper-13">EMAIL:</div>
-          <div className="text-wrapper-14">CALL US:</div>
-          <div className="text-wrapper-15">LOCATION:</div>
-          <div className="text-wrapper-16">mazingira.sustain@gmai.org</div>
-          <div className="text-wrapper-17">+254 721 526852</div>
-          <div className="text-wrapper-18">Park-Road, Kilimani</div>
-          <div className="rectangle-7" />
-          <div className="group-4">
-            <img className="location" alt="Location" src="src/assets/location-1.png" />
-            <div className="group-5">
-              <p className="MAZINGIRA">
-                <span className="text-wrapper-19">MAZI</span>
-                <span className="text-wrapper-20">NGIRA</span>
-              </p>
-              <div className="nature">Nature &amp; sustainability</div>
-            </div>
-            <img className="phone-receiver" alt="Phone receiver" src="src/assets/phone-receiver-silhouette-1.png" />
-            <img className="paper-plane" alt="Paper plane" src="src/assets/paper-plane-2-1.png" />
-            <div className="group-6">
-              <div className="overlap-group-3">
-                <img className="rectangle-8" alt="Rectangle" src="src/assets/rectangle-44.svg" />
-                <div className="group-7">
-                  <div className="text-wrapper-21">Home</div>
-                  <div className="text-wrapper-22">About</div>
-                  <div className="text-wrapper-23">Organizations</div>
-                  <div className="text-wrapper-24">Contact</div>
-                </div>
-                <img className="rectangle-9" alt="Rectangle" src="src/assets/rectangle-45.svg" />
-                <div className="text-wrapper-25">Donate</div>
-              </div>
-            </div>
-            <div className="rectangle-10" />
-          </div>
-          <div className="text-wrapper-26">Sign Up</div>
-          <div className="text-wrapper-27">Sign In</div>
-        </div>
-        <div className="group-8">
-          <img className="rectangle-11" alt="Rectangle" src="src/assets/rectangle-60.png" />
-          <div className="overlap-8">
-            <div className="rectangle-12" />
-            <p className="element-FUNDS-RAISED">
-              <span className="text-wrapper-10">
-                $920,000
-                <br />
-              </span>
-              <span className="text-wrapper-28">FUNDS RAISED BY 1200 PEOPLE</span>
-            </p>
-            <img className="heart" alt="Heart" src="src/assets/heart-1.png" />
-          </div>
-          <p className="p">
-            Environmental degradation is the disintegration of the earth or deterioration of the environment through the
-            consumption of assets, for example, air, water and soil; the destruction of environments and the eradication
-            of wildlife. It is characterized as any change or aggravation to nature’s turf seen to be pernicious or
-            undesirable. This process leads to the endangerment of the human race, however, there are limited resources
-            to help combat this at this time.
-          </p>
-          <p className="text-wrapper-29">Do You Care Our Mother Earth Like We Do?</p>
-        </div>
-        <div className="overlap-9">
-          <div className="rectangle-13" />
-          <img className="rectangle-14" alt="Rectangle" src="src/assets/rectangle-54.png" />
-          <div className="rectangle-15" />
-        <p className="text-wrapper-30">
-          Saving animals begins with a particular budget for buying the important provision.
-        </p>
-      </div>
-      <div className="overlap-10">
-        <div className="rectangle-16" />
-        <img className="hands-with-plants" alt="Hands with plants" src="src/assets/hands-with-plants-1280-1.png" />
-        <img className="instagram" alt="Instagram" src="src/assets/instagram-1.png" />
-        <img className="twitter-sign" alt="Twitter sign" src="src/assets/twitter-sign-1.png" />
-        <img className="facebook" alt="Facebook" src="src/assets/facebook-1.png" />
-        <div className="group-9">
-          <p className="MAZINGIRA-2">
-            <span className="text-wrapper-19">MAZI</span>
-            <span className="text-wrapper-20">NGIRA</span>
-          </p>
-          <div className="nature-2">Nature &amp; sustainability</div>
-        </div>
-        <p className="have-a-question-park">
-          <span className="text-wrapper-31">
-            Have a Question?
-            <br />
-          </span>
-          <span className="text-wrapper-32">
-            Park-Road, Kilimani
-            <br />
-            +254 721 526852
-            <br />
-            mazingira.sustain@gmai.org
-          </span>
-        </p>
-        <img className="rectangle-17" alt="Rectangle" src="src/assets/rectangle-62.png" />
-        <p className="text-wrapper-33">
-          A small river named Duden flows by their place and supplies it with the necessary regelialia.
-        </p>
-        <p className="services-recycling">
-          <span className="text-wrapper-34">
-            Services
-            <br />
-            Recycling
-            <br />
-            Water Refine
-            <br />
-            Ecosystem
-            <br />
-            Solar Enerfy
-          </span>
-        </p>
-        <p className="save-animals-plant">
-          Save Animals
-          <br />
-          Plant Ecology
-          <br />
-          Dynamic Ecology
-          <br />
-          Tree Planting
-        </p>
-      </div>
-      <div className="overlap-11">
-        <p className="text-wrapper-35">Copyright ©2023 All rights reserved |</p>
-      </div>
-      <div className="overlap-12">
-        <img className="hands-with-plants-2" alt="Hands with plants" src="src/assets/hands-with-plants-1280-2.png" />
-        <div className="hands-with-plants-3" />
-        <div className="text-wrapper-36">Subscribe For Newsletter</div>
-        <div className="rectangle-18" />
-        <div className="rectangle-19" />
-        <div className="text-wrapper-37">SUBSCRIBE NOW!</div>
-      </div>
-    </div>
+const TextWrapper = ({ className, children }) => <div className={className}>{children}</div>;
+
+const OverlapGroup = ({ imgSrc, imgAlt, text, goal }) => (
+  <div className="overlap-group">
+    <img className="img" alt={imgAlt} src={imgSrc} />
+    <TextWrapper className="text-wrapper">{text}</TextWrapper>
+    <p className="GROUP-GOAL">{goal}</p>
   </div>
 );
+
+const LandingPage = () => {
+  const overlapGroupsInfo = [
+    {
+      imgSrc: 'rectangle-51.png',
+      imgAlt: 'Rectangle',
+      text: 'Recycling is the Answer',
+      goal: 'GROUP GOAL: 26000.00 USD\nRAISED: 11500.00 USD (44.23%)',
+    },
+    // Add more objects for other overlap groups if necessary
+  ];
+
+  return (
+    <div className="landing-page">
+      <TextWrapper className="text-wrapper">TRANSFORM THE WORLD</TextWrapper>
+      {/* ... other content ... */}
+
+      {overlapGroupsInfo.map((group, index) => (
+        <OverlapGroup key={index} {...group} />
+      ))}
+
+      {/* ... other content ... */}
+
+      <footer>
+        <TextWrapper className="text-wrapper-35">
+          Copyright ©2023 All rights reserved |
+        </TextWrapper>
+      </footer>
+    </div>
+  );
 };
+
+export default LandingPage;
